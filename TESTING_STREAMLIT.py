305135707cce964502d46d22ed6ceceb269ebdaf
jazzfin21 <jacobson9393@gmail.com>
2026-03-05 09:51:23 -0500

    initial commit

---
+++ TESTING_STREAMLIT.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+
+
+# In[ ]:
+
+
+file = r"C:\Users\AJacobson1\Desktop\Transfer\Output\PricingOutput.csv"
+
+
+# In[ ]:
+
+
+df = pd.read_csv(file)
+
+
+# In[ ]:
+
+
+unique_payors = df['payer_name'].unique()
+selected_payor = st.selectbox('Select a payor to filer: ', unique_payors)
+
+filtered_df = df[df['payer_name'] == selected_payor]
+
+
+# In[ ]:
+
+
+st.title('Northwell Health Competitor Price Transparency Data')
+st.write('The table below contains hospital price transparency data from the latest machine-readable files published by Northwell Health competitor hospitals.')
+
+st.dataframe(df, use_container_width=True)
+
+
+# In[ ]:
+
+
+st.write('Here is the data for the payor you selected.')
+st.dataframe(filtered_df)
+
