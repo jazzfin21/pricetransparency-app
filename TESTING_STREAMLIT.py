3b3a7c9fd5800c960f536ff5ac70cbd551f9eae7
jazzfin21 <jacobson9393@gmail.com>
2026-03-05 14:27:11 -0500

    initial commit

--- TESTING_STREAMLIT.py
+++ TESTING_STREAMLIT.py
@@ -1,57 +1,57 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 import streamlit as st
 import pandas as pd
 
 
-# In[ ]:
+# In[2]:
 
 
 #file = "C:/Users/AJacobson1/Desktop/Transfer/Output/PricingOutput.csv"
 
 
-# In[ ]:
+# In[3]:
 
 
 file = 'https://raw.githubusercontent.com/jazzfin21/pricetransparency-app/refs/heads/main/PricingOutput.csv'
 
 
-# In[ ]:
+# In[4]:
 
 
 df = pd.read_csv(file, encoding='utf8')
 
 
-# In[ ]:
+# In[5]:
 
 
 df['payer_name'] = df['payer_name'].fillna('none')
 
 
-# In[ ]:
+# In[6]:
 
 
 for col in df.columns:
     df[col] = df[col].astype(str)
 
 
-# In[ ]:
+# In[7]:
 
 
 df = df.drop(columns=['A','0'])
 
 
-# In[ ]:
+# In[8]:
 
 
 #df['standard_charge_dollar'].dtype
 
 
-# In[ ]:
+# In[9]:
 
 
 unique_payors = df['payer_name'].unique()
@@ -60,18 +60,18 @@ selected_payor = st.selectbox('Select a payor to filer: ', unique_payors)
 filtered_df = df[df['payer_name'] == selected_payor]
 
 
-# In[ ]:
+# In[11]:
 
 
 st.title('Northwell Health Competitor Price Transparency Data')
 st.write('The table below contains hospital price transparency data from the latest machine-readable files published by Northwell Health competitor hospitals.')
 
-st.dataframe(df, use_container_width=True)
+st.write(df)
 
 
-# In[ ]:
+# In[12]:
 
 
 st.write('Here is the data for the payor you selected.')
-st.dataframe(filtered_df)
+st.write(filtered_df)
 
